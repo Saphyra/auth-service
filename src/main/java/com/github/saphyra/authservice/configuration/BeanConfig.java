e625d96a90c6b3d90ad2bccb004a33b191ecd7c7
Bernat Bonda <bondabear@gmail.com>
2019-01-02 01:17:52 +0100

    Config skeleton

---
+++ src/main/java/com/github/saphyra/authservice/configuration/BeanConfig.java
@@ -0,0 +1,7 @@
+package com.github.saphyra.authservice.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+}
--- src/main/java/com/github/saphyra/authservice/domain/Credentials.java
+++ src/main/java/com/github/saphyra/authservice/domain/Credentials.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class Credentials {
+    private String userId;
     private String userName;
     private String password;
 }
