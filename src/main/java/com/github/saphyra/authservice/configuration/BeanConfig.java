8ac35a53362f18cf33652cbf6f44b8b258a88dc0
Bernat Bonda <bondabear@gmail.com>
2019-01-02 01:48:15 +0100

    Component scan

--- src/main/java/com/github/saphyra/authservice/configuration/BeanConfig.java
+++ src/main/java/com/github/saphyra/authservice/configuration/BeanConfig.java
@@ -1,7 +1,11 @@
 package com.github.saphyra.authservice.configuration;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(com.github.saphyra.encryption.configuration.BeanConfig.class)
+@ComponentScan(basePackages = "com.github.saphyra.authservice")
 public class BeanConfig {
 }
